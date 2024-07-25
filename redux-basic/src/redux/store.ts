97c1c4e10bbe73220cf6ac23486b18edc2a5e944
Alex-DeveloperFS <iotov.developer@gmail.com>
2024-07-25 13:12:43 +0300

    upd2 commit

--- redux-basic/src/redux/store.ts
+++ redux-basic/src/redux/store.ts
@@ -1,21 +1,21 @@
-import {Action, configureStore, Dispatch, Middleware, MiddlewareAPI} from "@reduxjs/toolkit"
-import counterReducer, {initialState} from "./reduser.ts"
-import {CounterAction, CounterState, RootState} from "./types.ts";
+import { Action, configureStore, Dispatch, Middleware, MiddlewareAPI } from '@reduxjs/toolkit'
+import { CounterAction, CounterState, RootState } from './types.ts'
+import counterReducer, {initialState} from "./reduсer.ts";
 
 const logger: Middleware = (storeApi: MiddlewareAPI<Dispatch, RootState>) => (next) => (action) => {
   const prevState = storeApi.getState().counter.count
   const result = next(action)
   const nextState = storeApi.getState().counter.count
 
-  console.log(`Previous state: ${prevState}, \n\tDispatch action: ${action.type}, \n\t\tNext count: ${nextState}`)
+  console.log(
+    `Previous count: ${prevState} \n\tDispatch action: ${(action as CounterAction).type} \n\t\tNext count: ${nextState}`
+  )
   return result
 }
 
 const store = configureStore({
   reducer: {
-    counter: (state: CounterState = initialState, action: Action) => {
-      return counterReducer(state, action as CounterAction)
-    }
+    counter: (state: CounterState = initialState, action: Action) => counterReducer(state, action as CounterAction)
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
